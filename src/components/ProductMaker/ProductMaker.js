f2a54e0a08cdd46e99b1a3ac43ed07d95a753d28
nagromn <neverlanddu02@hotmail.fr>
2023-01-18 23:51:07 +0100

    adding bootstrap design and images

--- src/components/ProductMaker/ProductMaker.js
+++ src/components/ProductMaker/ProductMaker.js
@@ -3,15 +3,21 @@ import Head from "../../layouts/Head";
 import Select from "react-select";
 import styles from "./ProductMaker.module.css";
 import { nanoid } from "nanoid";
+
 // Data
 import base from "../../data/base.json";
 import components from "../../data/components.json";
+
+// Bootstrap components
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 export default function ProductMaker() {
   const [isSearchable, setIsSearchable] = useState(true);
   const [isClearable, setIsClearable] = useState(true);
   const [selectedBase, setSelectedBase] = useState();
   const [selectedComponent, setSelectedComponent] = useState([]);
+  const [submitted, setIsSubmitted] = useState(false);
 
   // Select des composants
   const componentItem = () => {
@@ -25,6 +31,7 @@ export default function ProductMaker() {
             label: component.name,
             value: nanoid(),
             compatibility: component.compatibility,
+            path: component.path,
           };
         })}
         isMulti
@@ -59,11 +66,7 @@ export default function ProductMaker() {
   // Gestion de l'input submit
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Reset selectedComponent si un composant a été sélectionné
-    // if (selectedComponent) {
-    //   setSelectedBase(null);
-    //   setSelectedComponent([]);
-    // }
+    setIsSubmitted(true);
     console.log(
       "Submitted Base :",
       selectedBase,
@@ -76,6 +79,7 @@ export default function ProductMaker() {
   const handleClear = () => {
     setSelectedBase(null);
     setSelectedComponent([]);
+    setIsSubmitted(false);
     console.log(
       "Deleted Base :",
       selectedBase,
@@ -89,7 +93,14 @@ export default function ProductMaker() {
       <Head title="Création de Produit"></Head>
       <h1>Création de produit</h1>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="base">Choix de la base</label>
+        <div className={styles.titleDisplay}>
+          <img
+            className={styles.iconSvg}
+            src={require("../../img/1.svg").default}
+            alt="1"
+          />
+          <h2>Choix de la base</h2>
+        </div>
         <div className={styles.productMaker}>
           {/* Select de la base */}
           <Select
@@ -109,18 +120,80 @@ export default function ProductMaker() {
             onChange={(e) => handleBaseChange(e)}
           />
         </div>
+
         {/* Si une base a été choisie, la condition permet d'afficher le componnentItem() */}
+
         {selectedBase ? (
-          <label htmlFor="component">Choix des composants</label>
+          <div>
+            <div className={styles.titleDisplay}>
+              <img
+                className={styles.iconSvg}
+                src={require("../../img/2.svg").default}
+                alt="2"
+              />
+              <h2>Choix des composants</h2>
+            </div>
+          </div>
         ) : null}
         {selectedBase ? (
           <div className={styles.productMaker}>
-            {componentItem(selectedComponent)}
-            {selectedComponent.length == selectedBase.slot &&
+            <div>{componentItem(selectedComponent)}</div>
+            {selectedComponent.length === selectedBase.slot &&
             selectedComponent ? (
-              <input type="submit" value="Envoyer" />
+              <Button className={styles.button} type="submit">
+                Valider
+              </Button>
             ) : null}
-            <input type="button" onClick={handleClear} value="Effacer" />
+            <Button
+              variant="danger"
+              className={styles.button}
+              onClick={handleClear}
+            >
+              Supprimer
+            </Button>
+          </div>
+        ) : null}
+
+        {/* Si une base et un composant ont été choisies, afficher les images des composants sélectionnés */}
+        {selectedBase && selectedComponent ? (
+          <div className={styles.productMaker}>
+            {selectedComponent.map((val) => (
+              <div className={styles.productMaker}>
+                <img src={val.path} alt={val.label} />
+              </div>
+            ))}
+          </div>
+        ) : null}
+
+        {/* Si le formulaire a été soumis, afficher la base et les composants sélectionnés */}
+        {submitted ? (
+          <div>
+            <div className={styles.titleDisplay}>
+              <img
+                className={styles.iconSvg}
+                src={require("../../img/3.svg").default}
+                alt="3"
+              />
+              <h2>Produit enregistré</h2>
+            </div>
+            <h3>{selectedBase.label}</h3>
+            <div className={styles.productMaker}>
+              {selectedComponent.map((val) => (
+                <div className={styles.row}>
+                  <div className={styles.column}>
+                    <Card className={styles.cardContainer}>
+                      <Card.Body>
+                        <Card.Img src={val.path} alt={val.label} />
+                        <Card.Text>{val.label}</Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <Button variant="danger" onClick={handleClear}>
+              Supprimer
+            </Button>
           </div>
         ) : null}
       </form>
